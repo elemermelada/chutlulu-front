5d3351967a64b7e7d897900747fc08a06e6249ff
elemermelada <iiasanmi9@gmail.com>
2024-05-05 16:54:38 +0200

    fix typo

--- public/api/proxy.php
+++ public/api/proxy.php
@@ -6,4 +6,4 @@ $password=$config["password"];
 $mode = $_GET["mode"];
 $input = $_GET["input"];
 
-echo file_get_contents("https://" . $user . ":" . $password . "@api.elemer.es/carmesyes/routes/proxy_request.php?port=8000&id=2&mode=" . $mode . "&input=" . $input)
+echo file_get_contents("https://" . $user . ":" . $password . "@api.elemer.es/carmesyes/routes/proxy_request.php?port=8000&id=2&mode=" . $mode . "&input=" . $input);
