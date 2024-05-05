32e4b1d3d129614e0c93a9f062626f8b042f1706
elemermelada <iiasanmi9@gmail.com>
2024-05-05 19:01:25 +0200

    fix post

--- public/api/proxy.php
+++ public/api/proxy.php
@@ -4,15 +4,15 @@ $user=$config["user"];
 $password=$config["password"];
 
 $mode = $_GET["mode"];
-$input = $_POST["input"];
+$content = file_get_contents("php://input");
 
 $opts = array('http' =>
-    array(
-        'method'  => 'POST',
-        'header'  => 'Content-Type: application/x-www-form-urlencoded',
-        'content' => $_POST
+array(
+    'method'  => 'POST',
+    'header'  => 'Content-Type: application/x-www-form-urlencoded',
+    'content' => $content
     )
 );
 $context  = stream_context_create($opts);
 
-echo file_get_contents("https://" . $user . ":" . $password . "@api.elemer.es/carmesyes/routes/proxy_request.php?port=8000&id=2&mode=" . $mode . "&input=" . $input, false, $context);
+echo file_get_contents("https://" . $user . ":" . $password . "@api.elemer.es/carmesyes/routes/proxy.php?port=8000&id=2&mode=" . $mode, false, $context);
