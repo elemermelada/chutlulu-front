9b084ac68cc43d49e023d8acb6cecfbd2ca2b659
elemermelada <iiasanmi9@gmail.com>
2024-05-05 17:10:12 +0200

    update api

--- src/api/dencode.ts
+++ src/api/dencode.ts
@@ -1,6 +1,14 @@
 export const DencodeTextAPI = async (mode:string, value:string) => {
     let url = (window as any).server_url as string  // Wacky
-    url += `&mode=${mode==="encode" ? 1 : 0}&input=${value}`
-    const result = await fetch(url)
+    url += `&mode=${mode==="encode" ? 1 : 0}`
+    const result = await fetch(url, {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, *cors, same-origin
+        headers: {
+            "Content-Type": "application/json",
+        },
+        referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify({input: value}), // body data type must match "Content-Type" header
+    })
     return result.text();
 }
