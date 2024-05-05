3a16cde43851245b05b29304066369d005b18faf
elemermelada <iiasanmi9@gmail.com>
2024-05-05 17:03:32 +0200

    simlper code

--- public/api/proxy.php
+++ public/api/proxy.php
@@ -1,9 +1,18 @@
 <?php
-$config = json_decode(file_get_contents("config.json"), true);
+$config = json_decode(file_get_contents("./config.json"), true);
 $user=$config["user"];
 $password=$config["password"];
 
 $mode = $_GET["mode"];
-$input = $_GET["input"];
+$input = $_POST["input"];
 
-echo file_get_contents("https://" . $user . ":" . $password . "@api.elemer.es/carmesyes/routes/proxy_request.php?port=8000&id=2&mode=" . $mode . "&input=" . $input);
+$opts = array('http' =>
+    array(
+        'method'  => 'POST',
+        'header'  => 'Content-Type: application/x-www-form-urlencoded',
+        'content' => $_POST
+    )
+);
+$context  = stream_context_create($opts);
+
+echo file_get_contents("https://" . $user . ":" . $password . "@api.elemer.es/carmesyes/routes/proxy_request.php?port=8000&id=2&mode=" . $mode . "&input=" . $input, false, $context);
